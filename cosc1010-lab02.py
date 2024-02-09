4c31d42016c63f6d9d5ea391100dae7fde7d6887
caiwal22 <159499059+caiwal22@users.noreply.github.com>
2024-02-08 17:46:58 -0700

    Add files via upload

---
+++ cosc1010-lab02.py
@@ -0,0 +1,42 @@
+# Caitlin Walling
+# UWYO COSC 1010
+# S2/8/2024
+# Lab 02 
+# Lab Section:40 
+# Sources, people worked with, help given to: Trai
+# your
+# comments
+# here
+
+## Section ONE
+
+# Complete the following print statement to print out "Hello, COSC1010"
+print()
+
+# Assign the string above to a variable named hello_message and print that variable
+hello_message = "Hello COSC1010"
+print("hello_message")
+
+# Assign the string "cowboy joe" to a variable, and print that variable with title casing
+string_one="cowboy"
+string_two="joe"
+full_string=f"{string_one}{string_two}"
+print("full_string")
+
+# Using f-strings and variables print out the solved solution for y = mx+b where:
+# x = 2
+# m = 1 
+# b = 13 
+# Your print message should be of the form "Given the variables m,x b we have y=m*x+b"
+# Where  the variables ae replaced with their values in the string
+
+# This one is more advanced then the others and will involve several steps 
+# Start by declaring the following variables:
+# x = 5
+# y = 7
+# z = 9 
+# print the sum of x,y,z
+# Print the sum of y raised to the z with the result multiplied by x
+# Print the result of raising z to the sum of x and y 
+# Finally create two strings "Cowboy " and "Joe" (spacing in cowboy intentional)
+# What happens if you add these two string together, assign it to a third variable, and print it
